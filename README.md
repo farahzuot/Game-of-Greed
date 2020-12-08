c0b5e992e2fa346c23b973349ed59c0892eff17d
Hadi Aji <hadeaji@gmail.com>
2020-12-08 18:01:12 +0200

    lab8 is too easy

--- README.md
+++ README.md
@@ -8,10 +8,22 @@ Created the banker class whish is responsable for holding the final result and h
 
 In this lab we prepared the standered game functionality and:
 
-- Application implement all features
-- Application simulate rolling between 1 and 6 dice
-- Application allow user to set aside dice each roll
-- Application allow “banking” current score or rolling again.
-- Application keep track of total score
-- Application keep track of current round
-- Application have automated tests to ensure proper operation
+- [x] Application implement all features
+- [x] Application simulate rolling between 1 and 6 dice
+- [x] Application allow user to set aside dice each roll
+- [x] Application allow “banking” current score or rolling again.
+- [x] Application keep track of total score
+- [x] Application keep track of current round
+- [x] Application have automated tests to ensure proper operation
+
+# Game Of Greed: Lab 3
+
+- [x]Application should implement features from versions 1 and 2
+- [x]Should handle setting aside scoring dice and continuing turn with remaining dice.
+- [x]Should handle when cheating occurs.
+  - [x]Or just typos.
+  - [x]E.g. roll = [1,3,5,2] and user selects 1, 1, 1, 1, 1, 1
+- [x]Should allow user to continue rolling with 6 new dice when all dice have scored in current turn.
+- [x]Handle zilch
+  - [x]No points for round, and round is over
+
--- game_of_greed/game.py
+++ game_of_greed/game.py
@@ -1,7 +1,8 @@
 from game_of_greed.game_logic import GameLogic , Banker
+from collections import Counter
 
 class Game:
-    def quitting(score):
+    def quitting(self,score):
         print(f'Total score is {score} points')
         print(f'Thanks for playing. You earned {score} points')
 
@@ -23,39 +24,97 @@ class Game:
             remaining_dice = 6
             score = 0
             dice_to_keep=0
+            past_roll_shelf=0
+            print(f'Starting round {round}')
             while dice_to_keep !='q':
-                print(f'Starting round {round}')
+                
                 print(f'Rolling {remaining_dice} dice...')
                 roll = self.roller(remaining_dice)
                 # Game.print_roll(roll)
-                print(','.join([str(i) for i in roll]))
-                dice_to_keep = input('Enter dice to keep (no spaces), or (q)uit: ')
+                ask = True
+                while ask == True:
+                    ask = False
+                    print(','.join([str(i) for i in roll]))
 
+                    if GameLogic.calculate_score(roll) ==0:
+                        print('Zilch!!! Round over')
+
+                        player1.shelved = 0
+                        player1.bank()
+                        print(f'You banked {player1.balance} points in round {round}')
+                        round +=1
+                        print(f'Total score is {score} points')
+                        remaining_dice = 6
+                        player1.balance =0
+                        print(f'Starting round {round}')
+                        dice_to_keep = 'Zilch'
+
+                        break
+
+                    dice_to_keep = input('Enter dice to keep (no spaces), or (q)uit: ')
+
+                    v =[int(x) for x in dice_to_keep if x.isdigit()]
+
+                    a = Counter(v).most_common()
+                    b = Counter(roll).most_common()
+                    votes=0
+
+                    cheatting = True
+                    if len(a) <= len(b):
+                        for i in a:
+                            for j in b:
+                                if i[0] == j[0]:
+                                    if i[1] <= j[1]:
+                                        votes +=1
+
+                    if len(a) == votes:
+                        cheatting = False
+
+                    if cheatting == True:
+                        print('Cheater!!! Or possibly made a typo...')
+                        ask = True
+                    
                 if dice_to_keep == 'q':
-                    Game.quitting(score) 
-                elif int(dice_to_keep) in roll:
-                    remaining_dice -=1
-                    curr = int(dice_to_keep)
-                    player1.shelf(GameLogic.calculate_score((curr,)))
+                    self.quitting(score)
+                elif dice_to_keep == 'Zilch':
+                    pass
+                else:  
+                    curr = tuple(v)
+                    for i in curr:
+                        remaining_dice -=1
+
+                    player1.shelf(GameLogic.calculate_score(curr))
+
+                    if past_roll_shelf != 0:
+                        player1.shelved += past_roll_shelf
+                        past_roll_shelf =0
 
                     print(f'You have {player1.shelved} unbanked points and {remaining_dice} dice remaining')
-                    player1.bank()
+                    
+                    if GameLogic.calculate_score(curr) == 1500:
+                        remaining_dice = 6
 
                     countinue = input(f'(r)oll again, (b)ank your points or (q)uit ')
                     if countinue == 'q':
+                        past_roll_shelf =0
                         dice_to_keep = 'q'
                         score += player1.balance
-                        Game.quitting(score)
+                        self.quitting(score)
                         player1.balance =0
 
                     if countinue == 'b':
+                        past_roll_shelf =0
+                        player1.bank()
                         score += player1.balance
                         print(f'You banked {player1.balance} points in round {round}')
                         round +=1
                         print(f'Total score is {score} points')
                         remaining_dice = 6
                         player1.balance =0
+                        print(f'Starting round {round}')
 
+                    if countinue == 'r':
+                        past_roll_shelf+=player1.shelved
 
 
 if __name__ == '__main__':
